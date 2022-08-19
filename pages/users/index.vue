06ffcfc33327ece22eac577a2555feeac44a5a7d
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-08-18 22:46:17 -0300

    Adding nested pages

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,22 +1,3 @@
 <template>
-    <div>
-        <h1>Put the id to load the user</h1>
-        <input type="text" v-model="userId"/>
-        <button @click="onLoadUser">Load User</button>
-    </div>
+    <h1>Put the id to load the user</h1>
 </template>
-
-<script>
-export default {
-    data () {
-        return {
-            userId: ''
-        }
-    },
-    methods: {
-        onLoadUser () {
-            this.$router.push(`/users/${this.userId}`)
-        }
-    }
-}
-</script>
