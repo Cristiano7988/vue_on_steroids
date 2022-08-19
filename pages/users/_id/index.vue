7cb39bc5828427493eb6c0d6df3905b8d19c8638
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-08-18 22:07:23 -0300

    Adding Nuxt validate method

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,11 @@
 <template>
     <h1>Hello, user {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+    validate (data) {
+        return /^\d+$/.test(data.params.id)
+    }
+}
+</script>
