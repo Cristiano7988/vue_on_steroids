f86c2129656de9565bb367fffe3173f4f7d4a175
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-08-25 19:49:40 -0300

    Spliting the Home into other component

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,66 @@
+<template>
+  <nuxt-link :to="'/posts/' + post.id">
+    <article class="post-preview">
+      <div class="post-thumbnail">
+        <img :src="post.thumbnail" />
+      </div>
+      <div class="post-content">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.texts[0]?.split('.')[0] }}.</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+    props: {
+        post: {
+            type: Object,
+            default: {}
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post-preview {
+  height: 300px;
+  position: relative;
+  overflow: hidden;
+  padding: 10px 0;
+  margin: 40px 0;
+}
+
+.post-thumbnail img {
+  position: relative;
+  left: 0%;
+}
+
+.post-content {
+  position: absolute;
+  background-color: rgba(999, 999, 999, .6);
+  width: 100%;
+  bottom: 0;
+  margin: 40px 0;
+  color: darkslategrey;
+  box-shadow: 0px 0px 0px black;
+  text-shadow: 0px 0px 0px black;
+}
+
+.post-preview:hover img {
+  width: 110%;
+  left: -10%;
+  transition: width 6s, left 6s;
+}
+
+.post-preview:hover .post-content {
+  box-shadow: 1px 1px 35px black;
+  text-shadow: 1px 1px 10px black;
+  transition: box-shadow 1s, text-shadow .2s;
+}
+
+.post-content p {
+  font-family: fantasy;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,30 +2,22 @@
   <div>
     <Title text="Get the latest tech news!" />
     <section class="featured-posts">
-      <nuxt-link v-for="({texts, title, id, thumbnail}, key) in posts" :key="key" :to="'/posts/'+id">
-        <article class="post-preview">
-          <div class="post-thumbnail">
-            <img :src="thumbnail" />
-          </div>
-          <div class="post-content">
-            <h1>{{ title }}</h1>
-            <p>{{ texts[0].split('.')[0] }}.</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview v-for="post in posts" :post="post" :key="key" />
     </section>
   </div>
 </template>
 
 <script>
 import posts from '~/data/posts.json';
+import PostPreview from '@/components/Posts/PostPreview.vue';
 
 export default {
     data() {
-      return {
-        posts
-      }
-    }
+        return {
+            posts
+        };
+    },
+    components: { PostPreview }
 }
 </script>
 
@@ -39,45 +31,5 @@ export default {
   .featured-posts {
     display: block;
   }
-}
-
-.post-preview {
-  height: 300px;
-  position: relative;
-  overflow: hidden;
-  padding: 10px 0;
-  margin: 40px 0;
-}
-
-.post-thumbnail img {
-  position: relative;
-  left: 0%;
-}
-
-.post-content {
-  position: absolute;
-  background-color: rgba(999, 999, 999, .6);
-  width: 100%;
-  bottom: 0;
-  margin: 40px 0;
-  color: darkslategrey;
-  box-shadow: 0px 0px 0px black;
-  text-shadow: 0px 0px 0px black;
-}
-
-.post-preview:hover img {
-  width: 110%;
-  left: -10%;
-  transition: width 6s, left 6s;
-}
-
-.post-preview:hover .post-content {
-  box-shadow: 1px 1px 35px black;
-  text-shadow: 1px 1px 10px black;
-  transition: box-shadow 1s, text-shadow .2s;
-}
-
-.post-content p {
-  font-family: fantasy;
 }
 </style>
