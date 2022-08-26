38148f85b6ccf25828e5f719fe4de7142e05fef4
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-08-25 22:22:48 -0300

    Spliting the post list from the Posts page

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul class="posts-list">
+        <li v-for="(post, index) in posts" :key="index">
+            <nuxt-link :to="'/posts/'+post.id">{{ post.title }}</nuxt-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+import posts from '~/data/posts.json';
+
+export default {
+    data() {
+        return {
+            posts
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .posts-list a {
+        color: darkslategray;
+        background: aliceblue;
+        display: block;
+        padding: 10px;
+        margin: 5px;
+        font-family: system-ui;
+    }
+    .posts-list a:hover {
+        background: chocolate;
+        color: white;
+        letter-spacing: 5px;
+        transition: background 1s, color .1s, letter-spacing 10s;
+    }
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,41 +2,16 @@
     <div>
         <Title text="Posts" />
         <section class="posts-page">
-            <ul class="posts-list">
-                <li v-for="(post, index) in posts" :key="index">
-                    <nuxt-link :to="'/posts/'+post.id">{{ post.title }}</nuxt-link>
-                </li>
-            </ul>
+            <PostList />
         </section>
     </div>
 </template>
-
 <script>
-import posts from '~/data/posts.json';
+import PostList from "~/components/Posts/PostList.vue";
 
 export default {
-    data() {
-        return {
-            posts
-        }
+    components: {
+        PostList
     }
 }
 </script>
-
-<style scoped>
-    .posts-list a {
-        color: darkslategray;
-        background: aliceblue;
-        display: block;
-        padding: 10px;
-        margin: 5px;
-        font-family: system-ui;
-    }
-    .posts-list a:hover {
-        background: chocolate;
-        color: white;
-        letter-spacing: 5px;
-        transition: background 1s, color .1s, letter-spacing 10s;
-    }
-</style>
-<
